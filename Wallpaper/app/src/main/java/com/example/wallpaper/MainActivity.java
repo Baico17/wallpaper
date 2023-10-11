1c7ec865cfd9b1c28b0ecf4ae4cef60f95d94138
Baico17 <bic199117@hotmail.com>
2023-10-11 11:52:40 +0200

    save api

--- Wallpaper/app/src/main/java/com/example/wallpaper/MainActivity.java
+++ Wallpaper/app/src/main/java/com/example/wallpaper/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //sMQ5rWviJ5ghwgoNRMylA5GHf06Z466GQgS1gla62ptnqU1B16kNHq2F   API
     }
 }
